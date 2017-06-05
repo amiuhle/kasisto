f6f68543787286d9b1096bd06451e8c0bc53ec6f
Timo Uhlmann <timouhlmann@gmail.com>
2017-06-05 15:25:45 +0200

    Basic layout using CSS Grid Layout
    Closes #7

--- src/components/App.js
+++ src/components/App.js
@@ -2,12 +2,11 @@ import React from 'react'
 
 export default (props) => {
   return (
-    <div className='App'>
-      <header>
-        <h1>Kasisto</h1>
-      </header>
-      <section />
-      <footer />
+    <div className='o-app'>
+      <h1 className='o-app__header u-margin-none'>Kasisto</h1>
+      <aside className='o-app__nav' />
+      <section className='o-app__content' />
+      <footer className='o-app__footer' />
     </div>
   )
 }
