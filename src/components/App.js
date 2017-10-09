b6c4ecac51073420f2a418b2ed38bc7fabf7b637
Timo Uhlmann <timouhlmann@gmail.com>
2017-10-09 19:43:17 -0300

    Monero Accepted Here!

--- src/components/App.js
+++ src/components/App.js
@@ -12,7 +12,6 @@ export default class App extends Component {
       <div className='o-app'>
         <div className='o-app__header'>
           <h1 className='u-margin-none u-margin-left o-flex__stretch'>Kasisto</h1>
-          <small className='u-muted'>Testnet</small>
         </div>
         <aside className='o-app__nav' />
         <HashRouter>
