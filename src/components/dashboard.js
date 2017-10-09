deb5ab69a9484b40d6e66930df07183d95d7905f
Timo Uhlmann <timouhlmann@gmail.com>
2017-10-09 19:43:17 -0300

    Monero Accepted Here!

--- src/components/dashboard.js
+++ src/components/dashboard.js
@@ -1,11 +1,14 @@
 import { func } from 'prop-types'
 import React from 'react'
 
+import moneroAccepted from '../images/monero-accepted-white-01.svg'
+
 const Dashboard = (props) => {
   const { onStartPayment } = props
 
   return (
-    <div>
+    <div className='o-flex o-flex--col' style={{ height: '100%' }}>
+      <img className='o-flex__stretch' src={moneroAccepted} />
       <div className='u-margin-bottom u-margin-top-large o-flex o-flex--col'>
         <button className='c-btn' onClick={onStartPayment}>
           Start Payment
