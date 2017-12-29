1771a6e535bb1b1d94fa5dc149811696ed432fa8
Timo Uhlmann <timouhlmann@gmail.com>
2017-12-28 19:56:56 -0500

    Fix DualCurrency specs

--- src/views/payments/__tests__/dual-currency.js
+++ src/views/payments/__tests__/dual-currency.js
@@ -1,13 +1,40 @@
 /* eslint-env jest */
 
 import React from 'react'
-import DualCurrency from '../dual-currency'
+import { Provider } from 'react-redux'
 import renderer from 'react-test-renderer'
+import {
+  reduxForm,
+  reducer as formReducer
+} from 'redux-form'
+import configureMockStore from 'redux-mock-store'
+import thunk from 'redux-thunk'
 
-xdescribe('DualCurrency', () => {
+import DualCurrency from '../dual-currency'
+
+const middlewares = [thunk]
+const mockStore = configureMockStore(middlewares)
+
+describe('DualCurrency', () => {
+  const createForm = reduxForm({
+    form: 'dummy'
+  })
+
   it('renders fiat and money', () => {
+    const store = mockStore({
+      form: formReducer,
+      settings: {}
+    })
+
+    const Form = createForm(({ handleSubmit }) => (
+      <form onSubmit={handleSubmit}>
+        <DualCurrency />
+      </form>
+    ))
     const component = renderer.create(
-      <DualCurrency />
+      <Provider store={store}>
+        <Form />
+      </Provider>
     )
 
     let tree = component.toJSON()
