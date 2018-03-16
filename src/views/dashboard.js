4dccbe56a6a9a0438fc2987ab9daa90263ce0962
Timo Uhlmann <timouhlmann@gmail.com>
2018-03-16 19:03:25 -0400

    Settings: Edit owner's logo

--- src/views/dashboard.js
+++ src/views/dashboard.js
@@ -13,8 +13,8 @@ const Dashboard = ({ onStartPayment, settings, lastPayment }) => (
     <div className='o-app__content c-dashboard'>
       <div className='c-dashboard--top'>
         <img
-          style={{ width: '60px', height: '60px', borderRadius: '50%', marginBottom: '10px' }}
-          src='https://s3-alpha.figma.com/img/b1e6/db2f/ca3b257e50393b55874c13d33f4daf70?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAJK6APQGEHTP6I3PA%2F20171222%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Date=20171222T194139Z&X-Amz-Expires=60&X-Amz-SignedHeaders=host&X-Amz-Signature=c7246ecb112f28d1874366ddb5c7eccb62b140f7f2bb7b25649b6c1df5a54362'
+          className='c-owner-logo u-margin-bottom-small'
+          src={settings.logo}
         />
         <h1 className='u-margin-none'>{settings.name || 'Coffee shop'}</h1>
         <small>{format(new Date(), 'DD MMMM YYYY')}</small>
--- src/views/settings.js
+++ src/views/settings.js
@@ -1,8 +1,13 @@
-import React, { Fragment } from 'react'
+import React, {
+  Component,
+  Fragment
+} from 'react'
 
 import { reduxForm, Field } from 'redux-form'
 
 import Icon from '../components/Icon'
+
+const { FileReader } = window
 
 const CMC_CURRENCIES = [
   'AUD',
@@ -39,54 +44,91 @@ const CMC_CURRENCIES = [
   'ZAR'
 ]
 
-const Settings = ({ handleSubmit }) => {
-  return (
-    <Fragment>
-      <div className='o-app__content c-settings'>
-        <form id='settings-form' onSubmit={handleSubmit}>
-          <div className='form-field'>
-            <label htmlFor='name'>Name</label>
-            <Field name='name' component='input' placeholder='Coffee shop' />
-          </div>
+class Settings extends Component {
+  state = {
+    logoPreview: null
+  }
 
-          <div className='form-field'>
-            <label htmlFor='fiatCurrency'>Currency</label>
-            <Field name='fiatCurrency' component='select'>
-              {CMC_CURRENCIES.map((currency) => <option key={currency}>{currency}</option>)}
-            </Field>
-          </div>
+  readImage = (e) => {
+    e.preventDefault()
+    const { change } = this.props
+    const reader = new FileReader()
+    const [ file ] = e.target.files
 
-          <div className='form-field'>
-            <label htmlFor='walletUrl'>Wallet URL</label>
-            <Field name='walletUrl' component='input' placeholder='https://testnet.kasisto.io:28084/json_rpc' />
-          </div>
+    reader.addEventListener('load', () => {
+      change('logo', reader.result)
+      this.setState({
+        logoPreview: reader.result
+      })
+    })
+    reader.readAsDataURL(file)
+  }
 
-          <div className='form-field'>
-            <label htmlFor='pollingInterval'>Polling interval (ms)</label>
-            <Field name='pollingInterval' component='input' type='number' placeholder='2000' />
-          </div>
+  render () {
+    const { handleSubmit, initialValues } = this.props
 
-          <div className='form-field'>
-            <label htmlFor='username'>Username</label>
-            <Field name='username' component='input' />
-          </div>
+    return (
+      <Fragment>
+        <div className='o-app__content c-settings'>
+          <form id='settings-form' onSubmit={handleSubmit}>
+            <div className='form-field u-margin-top-small o-flex o-flex--ai-center'>
+              <input id='logo' name='logo' type='file' accept='image/*'
+                style={{opacity: 0, height: 0}}
+                onChange={this.readImage}
+              />
+              <label htmlFor='logo' className='u-brand-primary u-medium u-base-size o-flex o-flex--col o-flex--ai-center'>
+                <img className='c-owner-logo u-margin-bottom-small'
+                  src={this.state.logoPreview || initialValues.logo}
+                />
+                Edit photo
+              </label>
+            </div>
 
-          <div className='form-field'>
-            <label htmlFor='password'>Password</label>
-            <Field name='password' component='input' type='password' />
-          </div>
-        </form>
-      </div>
+            <div className='form-field'>
+              <label htmlFor='name'>Name</label>
+              <Field name='name' component='input' placeholder='Coffee shop' />
+            </div>
 
-      <div className='o-app__top-left'>
-        <Icon href='/' name='back' />
-      </div>
+            <div className='form-field'>
+              <label htmlFor='fiatCurrency'>Currency</label>
+              <Field name='fiatCurrency' component='select'>
+                {CMC_CURRENCIES.map((currency) => <option key={currency}>{currency}</option>)}
+              </Field>
+            </div>
 
-      <button form='settings-form' className='o-app__footer c-btn'>
-        Save
-      </button>
-    </Fragment>
-  )
+            <div className='form-field'>
+              <label htmlFor='walletUrl'>Wallet URL</label>
+              <Field name='walletUrl' component='input' placeholder='https://testnet.kasisto.io:28084/json_rpc' />
+            </div>
+
+            <div className='form-field'>
+              <label htmlFor='pollingInterval'>Polling interval (ms)</label>
+              <Field name='pollingInterval' component='input' type='number' placeholder='2000' />
+            </div>
+
+            <div className='form-field'>
+              <label htmlFor='username'>Username</label>
+              <Field name='username' component='input' />
+            </div>
+
+            <div className='form-field'>
+              <label htmlFor='password'>Password</label>
+              <Field name='password' component='input' type='password' />
+            </div>
+          </form>
+        </div>
+
+        <div className='o-app__top-left'>
+          <Icon href='/' name='back' />
+        </div>
+        <div className='o-app__header'>Settings</div>
+
+        <button form='settings-form' className='o-app__footer c-btn'>
+          Save
+        </button>
+      </Fragment>
+    )
+  }
 }
 
 const createForm = reduxForm({
