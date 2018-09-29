454c02ad23a4fe05994289abc1695900a4d40ccf
Timo Uhlmann <timouhlmann@gmail.com>
2018-09-29 19:25:46 +0200

    Install and configure autoprefixer.
    Global browserslist config.

--- babel.config.js
+++ babel.config.js
@@ -1,13 +1,5 @@
 const presets = [
   ['@babel/preset-env', {
-    targets: {
-      chrome: '60',
-      firefox: '58',
-      safari: '10',
-      ios: '10',
-      edge: '13',
-      node: 'current'
-    },
     'debug': false,
     useBuiltIns: 'usage'
   }],
